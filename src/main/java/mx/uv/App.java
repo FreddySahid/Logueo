e3324b3bc2e70ebb33549fcdb9a6ff36acf3143f
FreddySahid <freddysahid@outlook.com>
2021-10-06 14:11:15 -0500

    Tengo un error con java

---
+++ src/main/java/mx/uv/App.java
@@ -0,0 +1,28 @@
+package mx.uv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+    }
+}
